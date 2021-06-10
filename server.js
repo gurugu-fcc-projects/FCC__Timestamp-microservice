75bef3c75036ec305ebe6eaa9a99aae45601b494
GuRuGu <peter.k.nets@gmail.com>
2021-06-10 12:15:28 +0300

    Add route to handle generic api call

--- server.js
+++ server.js
@@ -2,24 +2,29 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api", function (req, res) {
+  const timestamp = new Date();
 
-// your first API endpoint... 
+  res.json({ unix: timestamp.getTime(), utc: timestamp.toUTCString() });
+});
+
 app.get("/api/:date", function (req, res) {
   const { date } = req.params;
   let timestamp;
@@ -35,9 +40,7 @@ app.get("/api/:date", function (req, res) {
   res.json({ unix: timestamp.getTime(), utc: timestamp.toUTCString() });
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
